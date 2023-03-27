b1e4ffd5a27aaed6e50debce4e3a6853e8501eb7
Sabnaj_42 <sabnajislam786@gmail.com>
2023-03-28 04:26:16 +0600

    modified branch b1 from b2

--- app/src/test/java/Branch/simple_Branch_B1.java
+++ app/src/test/java/Branch/simple_Branch_B1.java
@@ -3,4 +3,18 @@ package Branch;
 public class simple_Branch_B1 {
     String name,id;
     float cgpa;
+    int number_of_classes;
+
+    public simple_Branch_B1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
